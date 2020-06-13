671af2ebfb5b9c02050a545f58b31825952556bc
shivang0131 <43657784+shivang0131@users.noreply.github.com>
2020-06-13 23:40:46 +0530

    Add files via upload
    This code will trace the route from your machine to the entered domain(.com/.org etc. ) or IP address.

---
+++ Tracing-route/trace-route.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h> //to use system()
+#include <string.h> //to use strcpy() and strcat()
+
+int main()
+{
+
+    char runCommand[] = "traceroute "; //use tracert for windows OS, traceroute -4 for ipV4 in windows
+    char trailCommand[] = " > trace.txt";
+
+
+    printf("\nEnter the domainname.com or IP address of the server\n");
+
+    char domain[100];
+    scanf("%s", domain);
+
+    strcat(runCommand, domain);
+    strcat(runCommand, trailCommand);
+    system(runCommand);
+
+
+    printf("\n Opening trace.txt....\n");
+    system("vi trace.txt"); //replace open with start in Windows OS.
+    
+    return 0;
+}
